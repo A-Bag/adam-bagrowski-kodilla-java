9f50d4b83bc6161e341837118f472ab1c4a16225
“A-Bag <bagrowski.adam@gmail.com>
2017-10-17 11:40:53 +0200

    Zadanie 6.5: mockujemy forum dyskusyjne

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
