9d74875f4c648b8a7507ad9d8ee011df711c5090
“A-Bag <bagrowski.adam@gmail.com>
2017-10-17 11:39:45 +0200

    Zadanie 6.5: mockujemy forum dyskusyjne

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -0,0 +1,30 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.Locale;
+
+public class ForumStatistics {
+    private int usersCount;
+    private int postsCount;
+    private int commentsCount;
+    private double avgPostsCountPerUser;
+    private double avgCommentsCountPerUser;
+    private double avgCommentsCountPerPost;
+
+    public void calculateAdvStatistics(Statistics statistics){
+        usersCount = statistics.getUsersNames().size();
+        postsCount = statistics.getPostsCount();
+        commentsCount = statistics.getCommentsCount();
+        avgPostsCountPerUser = usersCount != 0 ? (double)postsCount/usersCount : -1.0;
+        avgCommentsCountPerUser = usersCount != 0 ? (double)commentsCount/usersCount : -1.0;
+        avgCommentsCountPerPost = usersCount != 0 ? (double)commentsCount/postsCount : -1.0;
+    }
+
+    public String showStatistics () {
+        String statistics = "Users count: " + usersCount + "\nPosts count: " + postsCount + "\nComments count: " + commentsCount +
+                "\nAverage posts count per user: " + String.format(Locale.ROOT,"%.2f", avgPostsCountPerUser) +
+                "\nAverage comments count per user: " + String.format(Locale.ROOT,"%.2f", avgCommentsCountPerUser) +
+                "\nAverage comments count per post: " + String.format(Locale.ROOT,"%.2f", avgCommentsCountPerPost);
+        System.out.println(statistics);
+        return statistics;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ForumStatisticsTestSuite {
+    @Test
+    public void testCalculateAdvStatistics(){
+        //Arrange
+        Statistics statistics = mock(Statistics.class);
+        List<String> userNames = new ArrayList<>();
+        for(int i=0; i<100; i++){
+            userNames.add("name" + i);
+        }
+        when(statistics.getUsersNames()).thenReturn(userNames);
+        when(statistics.getCommentsCount()).thenReturn(100);
+        when(statistics.getPostsCount()).thenReturn(1000);
+
+        ForumStatistics forumStatistics = new ForumStatistics();
+        String expectedStatistics = "Users count: " + 100 + "\nPosts count: " + 1000 + "\nComments count: " + 100 +
+                "\nAverage posts count per user: " + "10.00" +
+                "\nAverage comments count per user: " + "1.00" +
+                "\nAverage comments count per post: " + "0.10";
+
+        //Act
+        forumStatistics.calculateAdvStatistics(statistics);
+
+        //Assert
+        Assert.assertEquals(expectedStatistics,forumStatistics.showStatistics());
+    }
+}
