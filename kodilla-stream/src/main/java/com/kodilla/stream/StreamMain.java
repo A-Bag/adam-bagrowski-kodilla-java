c61504d3118d085ed03f87d880e22e5ced183a96
“A-Bag <bagrowski.adam@gmail.com>
2017-10-18 18:04:10 +0200

    Zadanie 7.1: Upiekszacz tekstow

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,13 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
 public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("aAa", (text) -> "*" + text + "*");
+        poemBeautifier.beautify("aAa", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("aAa", (text) -> text.toLowerCase());
+        poemBeautifier.beautify("aAa", (text) -> text.substring(1,2));
+    }
 }
