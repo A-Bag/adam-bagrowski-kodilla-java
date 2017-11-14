c2e5d9babfa6e084f39a8593bc2bcddcb0ffdfb2
“A-Bag <bagrowski.adam@gmail.com>
2017-11-14 09:57:14 +0100

    Zadanie 10.3 v2

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -1,9 +1,6 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Board {
     TaskList toDoList;
     TaskList inProgressList;
