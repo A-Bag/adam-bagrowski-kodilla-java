f04038cd48fa3ba5d452dbf4aea90a37117f4905
“A-Bag <bagrowski.adam@gmail.com>
2017-11-16 09:14:58 +0100

    Zadanie 11.2

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/library/Book.java
@@ -0,0 +1,54 @@
+package com.kodilla.patterns.library;
+
+import java.time.LocalDate;
+
+public final class Book {
+    private final String title;
+    private final String author;
+    private final LocalDate publicationDate;
+
+    public Book(final String title, final String author, final LocalDate publicationDate) {
+        this.title = title;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationDate=" + publicationDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (!title.equals(book.title)) return false;
+        return author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        return result;
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/library/LibraryTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.patterns.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class LibraryTestSuite {
+    @Test
+    public void testGetBooks() {
+        //Arrange
+        Library library = new Library("library1");
+        for (int n=1; n<11; n++) {
+            library.getBooks().add(new Book("title" + n, "author" + n, LocalDate.now()));
+        }
+
+        Library shallowCopiedLibrary = null;
+        Library deepCopiedLibrary = null;
+        try {
+            shallowCopiedLibrary = library.shallowCopy();
+            deepCopiedLibrary = library.deepCopy();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        shallowCopiedLibrary.setName("library2");
+        deepCopiedLibrary.setName("library3");
+
+        //Act
+        library.getBooks().remove(new Book("title1", "author1", LocalDate.now()));
+
+        //Assert
+        Assert.assertEquals(9, library.getBooks().size());
+        Assert.assertEquals(9, shallowCopiedLibrary.getBooks().size());
+        Assert.assertEquals(10, deepCopiedLibrary.getBooks().size());
+        Assert.assertEquals(library.getBooks(), shallowCopiedLibrary.getBooks());
+        Assert.assertNotEquals(library.getBooks(), deepCopiedLibrary.getBooks());
+    }
+}
