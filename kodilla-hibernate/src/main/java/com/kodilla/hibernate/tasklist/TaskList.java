ade9c38b1772cef0a2601ae48459b9816cab96ad
“A-Bag <bagrowski.adam@gmail.com>
2017-12-05 12:26:14 +0100

    Problem: hbm2dll schema update

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
-@Access(AccessType.FIELD)
 public class TaskList {
     @Id
     @GeneratedValue
@@ -23,8 +22,13 @@ public class TaskList {
     @Column(name = "DESCRIPTION")
     private String description;
 
-    @Transient
-    private List<Task> list;
+    @OneToMany (
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -32,7 +36,6 @@ public class TaskList {
     public TaskList(String listName, String description) {
         this.listName = listName;
         this.description = description;
-        this.list = new ArrayList<>();
     }
 
     public String getListName() {
@@ -42,4 +45,67 @@ public class TaskList {
     public int getId() {
         return id;
     }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
 }
+
+/*public class TaskList {
+    private int id;
+    private String listName;
+    private String description;
+    private List<Task> tasks = new ArrayList<>();
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @OneToMany (
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}*/
