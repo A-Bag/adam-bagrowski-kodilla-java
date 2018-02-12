d19f69440c7bbf782d29087c6c0149bd99019b76
“A-Bag <bagrowski.adam@gmail.com>
2018-02-12 20:38:08 +0100

    22-3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -28,4 +28,24 @@ public class StoredProcTestSuite {
         Assert.assertEquals(0, howMany);
     }
 
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = -1";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall = "CALL updateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = -1";
+        int howMany = -1;
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
+
 }
